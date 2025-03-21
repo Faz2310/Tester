cabf69bb5ff1d0a2c98bdd12acaf9782a0673bc9
Faz2310 <142519568+Faz2310@users.noreply.github.com>
2025-03-21 12:54:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ st.title('Hello World')
 st.write('This Is A simple Streamlit App')
 
 st.chat_input("Enter Your Input")
+if input == 'what are you doing':
+    st.write('Im Doing Work')
 
 if st.button('Hello'):
     st.write("Hello from streamlit")
